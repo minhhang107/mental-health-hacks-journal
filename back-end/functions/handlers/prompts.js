2ea44a9425199d052b1affc8a9e4d4de53a85206
tuanthanh2067 <tuanthanh2067@gmail.com>
2021-05-22 09:40:41 -0400

    add delete journal & prompt

--- back-end/functions/handlers/prompts.js
+++ back-end/functions/handlers/prompts.js
@@ -77,3 +77,27 @@ exports.addPrompt = (req, res) => {
 };
 
 // delete prompt
+exports.deletePrompt = (req, res) => {
+  const document = db.doc(`/prompts/${req.params.promptId}`);
+  document
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return res.status(400).json({ error: "Prompt not found!" });
+      }
+      if (doc.data().userId !== req.user.userId) {
+        return res.status(403).json({ error: "Unauthorized" });
+      } else {
+        return document.delete();
+      }
+    })
+    .then(() => {
+      res.status(200).json({ message: "Prompt has been deleted successfully" });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res
+        .status(500)
+        .json({ error: "Something went wrong, please try again" });
+    });
+};
