b00038c1d1e40e3df66ba3e0df040f291fd9b63a
Phu Thong Pham <ptpham4@myseneca.ca>
2021-05-22 18:56:09 -0400

    Added error handling

--- back-end/functions/handlers/journals.js
+++ back-end/functions/handlers/journals.js
@@ -58,43 +58,49 @@ exports.getOneJournal = (req, res) => {
   }
 */
 exports.addJournal = async (req, res) => {
-  if (req.body.content.trim() === "") {
-    return res.status(400).json({ error: "Content must not be empty" });
+  try {
+    if (req.body.content.trim() === "") {
+      return res.status(400).json({ error: "Content must not be empty" });
+    }
+
+    const analysis = await analyze(req.body.content);
+
+    const newJournal = {
+      content: req.body.content,
+      userId: req.user.userId,
+      dateCreated: new Date().toISOString(),
+      moodStatus: req.body.moodStatus,
+      moodScore: analysis.score,
+    };
+
+    db.collection("journals")
+      .add(newJournal)
+      .then((doc) => {
+        const resJournal = newJournal;
+        resJournal.journalId = doc.id;
+
+        // Filter negative or mixed journal only when user allows
+        const isNegativeOrMixedScore =
+          analysis.score < 0 || (analysis.score == 0 && analysis.magnitude > 1);
+        if (req.body.allowPrompt && isNegativeOrMixedScore) {
+          resJournal.magnitude = analysis.magnitude;
+          resJournal.visible = req.body.allowPrompt;
+          // Create a prompt from negative content
+          addPrompt(resJournal);
+        }
+
+        res.status(200).json(resJournal);
+      })
+      .catch((err) => {
+        console.error(err);
+        res
+          .status(500)
+          .json({ error: "Something went wrong, please try again!" });
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Something went wrong, please try again!" });
   }
-
-  const analysis = await analyze(req.body.content);
-
-  const newJournal = {
-    content: req.body.content,
-    userId: req.user.userId,
-    dateCreated: new Date().toISOString(),
-    moodStatus: req.body.moodStatus,
-    moodScore: analysis.score
-  };
-
-  db.collection("journals")
-    .add(newJournal)
-    .then((doc) => {
-      const resJournal = newJournal;
-      resJournal.journalId = doc.id;
-
-      // Filter negative or mixed journal only when user allows
-      const isNegativeOrMixedScore = analysis.score < 0 || (analysis.score == 0 && analysis.magnitude > 1);
-      if (req.body.allowPrompt && isNegativeOrMixedScore) {
-        resJournal.magnitude = analysis.magnitude;
-        resJournal.visible = req.body.allowPrompt;
-        // Create a prompt from negative content
-        addPrompt(resJournal);
-      }
-
-      res.status(200).json(resJournal);
-    })
-    .catch((err) => {
-      console.error(err);
-      res
-        .status(500)
-        .json({ error: "Something went wrong, please try again!" });
-    });
 };
 
 // delete journal
